84d3708864495e71800ab1eeedfdd1a39bf1774b
amichon <antoine@hashbang.coop>
2026-09-01 13:24:38 +0200

    feat: Ajout d'un modele BandeauInformation

--- mesads/newsletters/models.py
+++ mesads/newsletters/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from solo.models import SingletonModel
 
 
 class NewsLetter(models.Model):
@@ -14,3 +15,17 @@ class NewsLetter(models.Model):
 
     def __str__(self):
         return f"Newsletter du {self.newsletter_date.strftime('%d/%m/%Y')}"
+
+
+class BandeauInformation(SingletonModel):
+    visible = models.BooleanField(verbose_name="Visibilité bandeau", default=False)
+    titre = models.CharField(
+        verbose_name="Titre", max_length=128, default="", blank=True
+    )
+    message = models.TextField(verbose_name="Message", default="", blank=True)
+
+    class Meta:
+        verbose_name = "Bandeau d'information"
+
+    def __str__(self):
+        return "Bandeau d'information"
